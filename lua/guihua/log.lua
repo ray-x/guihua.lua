82a0726261db99ef24d6ddc466ac611cae542dbe
ray-x <rayx.cn@gmail.com>
2021-05-13 20:16:53 +1000

    revert log level

--- lua/guihua/log.lua
+++ lua/guihua/log.lua
@@ -17,7 +17,7 @@ local default_config = {
   -- Should write to a file
   use_file = true,
   -- Any messages above this level will be logged.
-  level = "info",
+  level = "error",
   -- Level configuration
   modes = {
     {name = "trace", hl = "Comment"},
