df0e77d0e3916f4c59a87b2a1ef502da9f9bada6
ray-x <rayx.cn@gmail.com>
2021-12-09 11:44:13 +1100

    high level api gui.lua

--- lua/guihua/floating.lua
+++ lua/guihua/floating.lua
@@ -1,13 +1,13 @@
 local api = vim.api
-local location = require "guihua.location"
+local location = require('guihua.location')
 local validate = vim.validate
 
-local log = require"guihua.log".info
-local trace = require"guihua.log".trace
-local columns = api.nvim_get_option("columns")
-local lines = api.nvim_get_option("lines")
-local shell = api.nvim_get_option("shell")
-local shellcmdflag = api.nvim_get_option("shellcmdflag")
+local log = require('guihua.log').info
+local trace = require('guihua.log').trace
+local columns = api.nvim_get_option('columns')
+local lines = api.nvim_get_option('lines')
+local shell = api.nvim_get_option('shell')
+local shellcmdflag = api.nvim_get_option('shellcmdflag')
 
 -- Create a simple floating terminal.
 local function floating_buf(opts) -- win_width, win_height, x, y, loc, prompt, enter, ft)
@@ -18,119 +18,123 @@ local function floating_buf(opts) -- win_width, win_height, x, y, loc, prompt, e
   -- if opts.border == "single" then
   --   opts.border = {}
   -- end
-  log("loc", opts.loc, opts.win_width, opts.win_height, x, y, enter, opts.ft)
+  log('loc', opts.loc, opts.win_width, opts.win_height, x, y, enter, opts.ft)
   -- win_w, win_h, x, y should be passwd in from view
   local loc = opts.loc or location.center
   local row, col = loc(opts.win_height, opts.win_width)
   local win_opts = {
-    style = opts.style or "minimal",
+    style = opts.style or 'minimal',
     width = opts.win_width or 80,
     height = opts.win_height or 20,
-    border = opts.border or "single" -- "shadow"
+    border = opts.border or 'single', -- "shadow"
   }
 
   if opts.external then
     win_opts.external = true
   else
-    win_opts.relative = opts.relative or "editor"
-    win_opts.bufpos = {0, 0}
+    win_opts.relative = opts.relative or 'editor'
+    win_opts.bufpos = { 0, 0 }
   end
 
-  if win_opts.relative == "editor" then
+  if win_opts.relative == 'editor' then
     win_opts.row = row + y
     win_opts.col = col + x
   end
-  log("floating", win_opts, opts)
+  log('floating', win_opts, opts)
   local buf = api.nvim_create_buf(false, true)
-  api.nvim_buf_set_option(buf, "bufhidden", "wipe")
-  api.nvim_buf_set_option(buf, "buflisted", false)
+  api.nvim_buf_set_option(buf, 'bufhidden', 'wipe')
+  api.nvim_buf_set_option(buf, 'buflisted', false)
   -- api.nvim_buf_set_option(buf, 'buftype', 'guihua_input')
   if prompt then
-    vim.fn.prompt_setprompt(buf, " ")
-    api.nvim_buf_set_option(buf, "buftype", "prompt") -- vim.fn.setbufvar(bufnr, "buflisted", 0)
+    vim.fn.prompt_setprompt(buf, ' ')
+    api.nvim_buf_set_option(buf, 'buftype', 'prompt') -- vim.fn.setbufvar(bufnr, "buflisted", 0)
   else
-    api.nvim_buf_set_option(buf, "readonly", true)
-    vim.api.nvim_buf_set_option(buf, "filetype", "guihua") -- default ft for all buffers. do not use specific ft e.g
+    api.nvim_buf_set_option(buf, 'readonly', true)
+    vim.api.nvim_buf_set_option(buf, 'filetype', 'guihua') -- default ft for all buffers. do not use specific ft e.g
     -- javascript as it may cause lsp loading
   end
   local win = api.nvim_open_win(buf, enter, win_opts)
-  log("creating win", win, "buf", buf)
+  log('creating win', win, 'buf', buf)
 
   -- note: if failed to focus on the view, you can add to the caller
   -- vim.fn.win_gotoid(win)
 
   -- api.nvim_buf_set_option(buf, "nobuflisted")
-  return buf, win, function()
-    if win == nil then
-      -- already closed or not valid
-      return
+  return buf,
+    win,
+    function()
+      if win == nil then
+        -- already closed or not valid
+        return
+      end
+      log('floatwin closing ', win)
+      if vim.api.nvim_win_is_valid(win) then
+        vim.api.nvim_win_close(win, true)
+        win = nil
+      end
     end
-    log("floatwin closing ", win)
-    if vim.api.nvim_win_is_valid(win) then
-      vim.api.nvim_win_close(win, true)
-      win = nil
-    end
-  end
 end
 
 -- Create a mask.
 local function floating_buf_mask(transparency) -- win_width, win_height, x, y, loc, prompt, enter, ft)
-  vim.validate({transparency = {transparency, 'number'}})
-  columns = api.nvim_get_option("columns")
-  lines = api.nvim_get_option("lines")
+  vim.validate({ transparency = { transparency, 'number' } })
+  columns = api.nvim_get_option('columns')
+  lines = api.nvim_get_option('lines')
   local loc = location.center
   local row, col = loc(lines, columns)
   local win_opts = {
-    style = "minimal",
-    relative = "editor",
+    style = 'minimal',
+    relative = 'editor',
     width = columns,
     height = lines,
-    bufpos = {0, 0},
-    zindex = 1 -- on bottom
+    bufpos = { 0, 0 },
+    zindex = 1, -- on bottom
   }
 
   local buf = api.nvim_create_buf(false, true)
-  api.nvim_buf_set_option(buf, "bufhidden", "wipe")
+  api.nvim_buf_set_option(buf, 'bufhidden', 'wipe')
   -- api.nvim_buf_set_option(buf, 'buftype', 'guihua_input')
-  api.nvim_buf_set_option(buf, "readonly", true)
-  api.nvim_buf_set_option(buf, "buflisted", false)
-  vim.api.nvim_buf_set_option(buf, "filetype", "guihua") -- default ft for all buffers. do not use specific ft e.g
+  api.nvim_buf_set_option(buf, 'readonly', true)
+  api.nvim_buf_set_option(buf, 'buflisted', false)
+  vim.api.nvim_buf_set_option(buf, 'filetype', 'guihua') -- default ft for all buffers. do not use specific ft e.g
   -- javascript as it may cause lsp loading
   local win = api.nvim_open_win(buf, false, win_opts)
-  api.nvim_win_set_option(win, "winblend", transparency)
-  log("creating win", win, "buf", buf)
+  api.nvim_win_set_option(win, 'winblend', transparency)
+  log('creating win', win, 'buf', buf)
 
-  return buf, win, function(...)
-    if win == nil then
-      -- already closed or not valid
-      return
+  return buf,
+    win,
+    function(...)
+      if win == nil then
+        -- already closed or not valid
+        return
+      end
+      log('floatwin closing ', win)
+      if vim.api.nvim_win_is_valid(win) then
+        vim.api.nvim_win_close(win, true)
+        win = nil
+      end
     end
-    log("floatwin closing ", win)
-    if vim.api.nvim_win_is_valid(win) then
-      vim.api.nvim_win_close(win, true)
-      win = nil
-    end
-  end
 end
 
 -- prepare buf and win for floatterm
 local function floatterm(opts)
   local buf = api.nvim_create_buf(false, true)
-  api.nvim_buf_set_keymap(buf, "t", "<ESC>", "<C-\\><C-c>", {})
-  api.nvim_buf_set_option(buf, "bufhidden", "wipe")
-  api.nvim_buf_set_option(buf, "buflisted", false)
+  api.nvim_buf_set_keymap(buf, 't', '<ESC>', '<C-\\><C-c>', {})
+  api.nvim_buf_set_option(buf, 'bufhidden', 'wipe')
+  api.nvim_buf_set_option(buf, 'buflisted', false)
 
   log(opts)
   local width = opts.win_width or math.floor(columns * 0.9)
   local height = opts.win_height or math.floor(lines * 0.9)
   local win_opts = {
-    relative = "editor",
-    style = "minimal",
+    relative = 'editor',
+    style = 'minimal',
     row = opts.y or math.floor((lines - height) * 0.5),
     col = opts.x or math.floor((columns - width) * 0.5),
     width = width,
     height = height,
-    border = opts.border
+    border = opts.border,
   }
 
   if opts.external then
@@ -152,8 +156,8 @@ local function floating_term(opts) -- cmd, callback, win_width, win_height, x, y
   -- opts.x = opts.x or 1
   -- opts.y = opts.y or 1
 
-  if opts.cmd == "" or opts.cmd == nil then
-    opts.cmd = vim.api.nvim_get_option("shell")
+  if opts.cmd == '' or opts.cmd == nil then
+    opts.cmd = vim.api.nvim_get_option('shell')
   end
 
   -- get dimensions
@@ -162,20 +166,20 @@ local function floating_term(opts) -- cmd, callback, win_width, win_height, x, y
 
   -- calculate our floating window size
 
-  columns = api.nvim_get_option("columns")
-  lines = api.nvim_get_option("lines")
+  columns = api.nvim_get_option('columns')
+  lines = api.nvim_get_option('lines')
   opts.win_height = opts.win_height or math.ceil(lines * 0.88)
   opts.win_width = opts.win_width or math.ceil(columns * 0.88)
   local win, buf, _ = floatterm(opts)
 
-  api.nvim_win_set_option(win, "winhl", "Normal:Normal")
+  api.nvim_win_set_option(win, 'winhl', 'Normal:Normal')
 
   local closer = function(...)
-    log("floatwin closing ", win)
+    log('floatwin closing ', win)
 
     if opts.autoclose ~= false then
       if vim.api.nvim_buf_is_valid(buf) then
-        vim.api.nvim_buf_delete(buf, {force = true})
+        vim.api.nvim_buf_delete(buf, { force = true })
       end
       if vim.api.nvim_win_is_valid(win) then
         vim.api.nvim_win_close(win, true)
@@ -186,9 +190,9 @@ local function floating_term(opts) -- cmd, callback, win_width, win_height, x, y
       opts.closer(opts.closer_args)
     end
   end
-  vim.api.nvim_buf_set_option(buf, "filetype", "guihua") -- default ft for all buffers. do not use specific ft e.g
+  vim.api.nvim_buf_set_option(buf, 'filetype', 'guihua') -- default ft for all buffers. do not use specific ft e.g
 
-  local args = {shell, shellcmdflag, opts.cmd}
+  local args = { shell, shellcmdflag, opts.cmd }
 
   vim.fn.termopen(args, {
     on_exit = function(_, _, _)
@@ -198,10 +202,51 @@ local function floating_term(opts) -- cmd, callback, win_width, win_height, x, y
       -- if callback then
       --   callback(lines)
       -- end
-    end
+    end,
   })
 
-  vim.cmd("startinsert!")
+  vim.cmd('startinsert!')
+  return buf, win, closer
+end
+
+-- wraper for tui/gui
+local term = function(opts)
+  local columns = api.nvim_get_option('columns')
+  local lines = api.nvim_get_option('lines')
+
+  local win_width, win_height
+  if columns > (opts.vsplit or 130) then
+    -- split in right
+    win_height = math.ceil(lines * 0.98)
+    win_width = math.ceil(columns * (opts.vwidth_rator or 0.5))
+
+    opts.y = win_height
+    opts.x = columns - win_width
+  else
+    win_height = math.ceil(lines * (opts.sheight_ratio) or 0.6  )
+    win_width = math.ceil(columns * 0.98)
+
+    opts.y = opts.y or lines - win_height
+    opts.x = opts.x or 1
+  end
+  opts.win_height = opts.win_height or win_height
+  opts.win_width = opts.win_width or win_width
+  opts.border = opts.border or 'single'
+  if opts.autoclose == nil then
+    opts.autoclose = true
+  end
+
+  opts.closer = function(args)
+    utils.log('closer callback')
+    vim.cmd('set noconfirm')
+    vim.cmd('bufdo e!')
+    vim.cmd('set confirm')
+  end
+  opts.closer_args = {}
+  local buf, win, closer = floating_term(opts)
+  api.nvim_command('setlocal nobuflisted')
+  api.nvim_buf_set_var(buf, opts.term_name or 'guihua_floating_term', { buf, win })
+
   return buf, win, closer
 end
 
@@ -212,27 +257,27 @@ local function test(prompt)
     x = 5,
     y = 5,
     prompt = prompt,
-    focus = true
+    focus = true,
   })
-  local data = {"floating buf", "line1", "line2", "line3", "line4", "line5"}
+  local data = { 'floating buf', 'line1', 'line2', 'line3', 'line4', 'line5' }
   for i = 1, 10, 1 do
-    vim.api.nvim_buf_set_lines(b, i, -1, false, {data[i]})
+    vim.api.nvim_buf_set_lines(b, i, -1, false, { data[i] })
   end
   -- vim.cmd('silent buffer ' .. tostring(b))
   -- vim.fn.win_gotoid(w)
   if prompt == true then
-    vim.cmd("startinsert!")
+    vim.cmd('startinsert!')
   end
 end
 
 local function test2(prompt)
-  local b, w, c = floating_buf({win_width = 30, win_height = 8, x = 25, y = 25, prompt = prompt})
-  local data = {"floating buf", "linea", "lineb", "linec", "lined", "linee"}
+  local b, w, c = floating_buf({ win_width = 30, win_height = 8, x = 25, y = 25, prompt = prompt })
+  local data = { 'floating buf', 'linea', 'lineb', 'linec', 'lined', 'linee' }
   for i = 1, 10, 1 do
-    vim.api.nvim_buf_set_lines(b, i, -1, false, {data[i]})
+    vim.api.nvim_buf_set_lines(b, i, -1, false, { data[i] })
   end
   if prompt == true then
-    vim.cmd("startinsert!")
+    vim.cmd('startinsert!')
   end
 end
 
@@ -251,5 +296,6 @@ end
 return {
   floating_buf = floating_buf,
   floating_term = floating_term,
-  floating_buf_mask = floating_buf_mask
+  floating_buf_mask = floating_buf_mask,
+  gui_term = term,
 }
---
+++ lua/guihua/gui.lua
@@ -0,0 +1,208 @@
+local M = {}
+local ListView = require('guihua.listview')
+local TextView = require('guihua.textview')
+local util = require('navigator.util')
+local log = require('navigator.util').log
+local trace = require('navigator.util').trace
+local api = vim.api
+
+local top_center = require('guihua.location').top_center
+
+local path_sep = require('navigator.util').path_sep()
+local path_cur = require('navigator.util').path_cur()
+function M._preview_location(opts) -- location, width, pos_x, pos_y
+  local uri = opts.uri
+  if uri == nil then
+    log('invalid/nil uri ')
+    return
+  end
+  local bufnr = vim.uri_to_bufnr(uri)
+  if not api.nvim_buf_is_loaded(bufnr) then
+    vim.fn.bufload(bufnr)
+  end
+  --
+
+  local display_range = opts.location.range
+  local syntax = api.nvim_buf_get_option(bufnr, 'ft')
+  if syntax == nil or #syntax < 1 then
+    syntax = 'c'
+  end
+
+  -- trace(syntax, contents)
+  local win_opts = {
+    syntax = syntax,
+    width = opts.width,
+    height = display_range['end'].line - display_range.start.line + 1,
+    preview_height = opts.height or opts.preview_height,
+    pos_x = opts.offset_x,
+    pos_y = opts.offset_y,
+    range = opts.range,
+    display_range = display_range,
+    uri = uri,
+    allow_edit = opts.enable_edit,
+  }
+
+  if opts.external then
+    win_opts.external = true
+    win_opts.relative = nil
+  end
+  -- win_opts.items = contents
+  win_opts.hl_line = opts.lnum - display_range.start.line
+  if win_opts.hl_line < 0 then
+    win_opts.hl_line = 1
+  end
+  trace(opts.lnum, opts.range.start.line, win_opts.hl_line)
+  log(win_opts)
+  local w = TextView:new({
+    loc = 'offset_center',
+    rect = {
+      height = win_opts.height, -- opts.preview_heigh or 12, -- TODO 12
+      width = win_opts.width,
+      pos_x = win_opts.pos_x,
+      pos_y = win_opts.pos_y,
+    },
+    list_view_height = win_opts.height,
+    -- data = display_data,
+    relative = win_opts.relative,
+    -- data = opts.items, -- either items or uri
+    uri = win_opts.uri,
+    syntax = win_opts.syntax,
+    enter = win_opts.enter or false,
+    range = win_opts.range,
+    border = opts.border,
+    display_range = win_opts.display_range,
+    hl_line = win_opts.hl_line,
+    allow_edit = win_opts.allow_edit,
+    external = win_opts.external,
+  })
+  return w
+end
+
+function M.preview_uri(opts) -- uri, width, line, col, offset_x, offset_y
+  -- local handle = vim.loop.new_async(vim.schedule_wrap(function()
+  local line_beg = (opts.lnum or 2) - 1
+  if line_beg >= opts.preview_lines_before or 1 then
+    line_beg = line_beg - (opts.preview_lines_before or 1)
+  elseif line_beg >= 2 then
+    line_beg = line_beg - 2
+  end
+  local loc = { uri = opts.uri, range = { start = { line = line_beg } } }
+
+  -- TODO: preview height
+  loc.range['end'] = { line = opts.lnum + opts.preview_height }
+  opts.location = loc
+
+  trace('uri', opts.uri, opts.lnum, opts.location.range.start.line, opts.location.range['end'].line)
+  return M._preview_location(opts)
+end
+
+function M.new_list_view(opts)
+  local items = opts.items
+  local data = opts.data or {}
+
+  local wwidth = api.nvim_get_option('columns')
+
+  local loc = 'top_center'
+
+  local mwidth = opts.min_width or 30
+  local width = math.floor(wwidth * mwidth)
+  if opts.width_ratio ~= nil and opts.width_ratio > 0.3 and opts.width_ratio < 0.99 then
+    width = math.floor(wwidth * opts.width_ratio)
+  end
+  width = math.min(120, width, opts.width or 120)
+  local wheight = math.floor(1 + api.nvim_get_option('lines') * (opts.height_ratio + opts.preview_height_ratio))
+  local pheight = math.max(#opts.data, math.floor(api.nvim_get_option('lines') * (opts.preview_height_ratio or 1)))
+  local prompt = opts.prompt or false
+  if opts.rawdata then
+    data = items
+  end
+
+  local border = opts.border or 'shadow'
+
+  if not data or vim.tbl_isempty(data) then
+    return
+  end
+
+  -- replace
+  -- TODO: 10 vimrc opt
+  if #data > 10 and opts.prompt == nil then
+    loc = 'top_center'
+    prompt = true
+  end
+
+  local lheight = math.min(#data, math.floor(wheight * (opts.min_height or 15)))
+
+  local r, _ = top_center(lheight, width)
+
+  local offset_y = r + lheight
+  -- style shadow took 1 lines
+  if border ~= 'none' then
+    if border == 'shadow' then
+      offset_y = offset_y + 1
+    else
+      offset_y = offset_y + 1 -- single?
+    end
+  end
+  -- if border is not set, this should be r+lheigh
+  if prompt then
+    offset_y = offset_y + 1 -- need to check this out
+  end
+  local idx = require('guihua.util').fzy_idx
+  local transparency = opts.transparency
+  if transparency == 100 then
+    transparency = nil
+  end
+  local ext = opts.external or false
+  if ext then
+    opts.relative = nil
+  end
+  return ListView:new({
+    loc = loc,
+    prompt = prompt,
+    relative = opts.relative,
+    style = opts.style,
+    api = opts.api,
+    total = opts.total,
+    rect = { height = lheight, width = width, pos_x = 0, pos_y = 0 },
+    -- preview_height = pheight,
+    ft = opts.ft or 'guihua',
+    -- data = display_data,
+    data = data,
+    border = border,
+    external = ext,
+    on_confirm = opts.on_confirm or function(item, split_opts)
+      log(split_opts)
+      split_opts = split_opts or {}
+      if item.filename ~= nil then
+        log('openfile ', item.filename, item.lnum, item.col)
+        util.open_file_at(item.filename, item.lnum, item.col, split_opts.split)
+      end
+    end,
+    transparency = transparency,
+    on_move = opts.on_move or function(item)
+      trace('on move', item)
+      trace('on move', item.text or item, item.uri, item.filename)
+      -- todo fix
+      if item.uri == nil then
+        item.uri = 'file:///' .. item.filename
+      end
+      return M.preview_uri({
+        uri = item.uri,
+        preview_lines_before = opts.preview_lines_before or 3,
+        width = width,
+        height = lheight, -- this is to cal offset
+        preview_height = pheight,
+        lnum = item.lnum,
+        col = item.col,
+        range = item.range,
+        offset_x = 0,
+        offset_y = offset_y,
+        border = border,
+        external = ext,
+        enable_edit = opts.enable_preview_edit or false,
+      })
+    end,
+  })
+end
+
+return M
