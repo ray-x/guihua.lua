0306a6c5f66c2b097978f895eede3510a80a9f7d
ray-x <rayx.cn@gmail.com>
2022-01-13 15:47:29 +1100

    support for windows

--- lua/guihua/log.lua
+++ lua/guihua/log.lua
@@ -8,7 +8,7 @@
 -- User configuration section
 local default_config = {
   -- Name of the plugin. Prepended to log messages
-  plugin = "gh", --  ',
+  plugin = 'gh', --  ',
   -- Should print the output to neovim while running
   use_console = false,
   -- Should highlighting be used in console (using echohl)
@@ -16,15 +16,18 @@ local default_config = {
   -- Should write to a file
   use_file = true,
   -- Any messages above this level will be logged.
-  level = "error",
+  level = 'error',
   -- Level configuration
   modes = {
-    {name = "trace", hl = "Comment"}, {name = "debug", hl = "Comment"},
-    {name = "info", hl = "None"}, {name = "warn", hl = "WarningMsg"},
-    {name = "error", hl = "ErrorMsg"}, {name = "fatal", hl = "ErrorMsg"}
+    { name = 'trace', hl = 'Comment' },
+    { name = 'debug', hl = 'Comment' },
+    { name = 'info', hl = 'None' },
+    { name = 'warn', hl = 'WarningMsg' },
+    { name = 'error', hl = 'ErrorMsg' },
+    { name = 'fatal', hl = 'ErrorMsg' },
   },
   -- Can limit the number of decimals displayed for floats
-  float_precision = 0.01
+  float_precision = 0.01,
 }
 
 -- {{{ NO NEED TO CHANGE
@@ -32,13 +35,23 @@ local log = {}
 
 local unpack = unpack or table.unpack
 
+local os_name = vim.loop.os_uname().sysname
+local is_windows = os_name == 'Windows' or os_name == 'Windows_NT'
+-- Check whether current buffer contains main function
+
+local function sep()
+  if is_windows then
+    return '\\'
+  end
+  return '/'
+end
+
 log.new = function(config, standalone)
-  config = vim.tbl_deep_extend("force", default_config, config)
+  config = vim.tbl_deep_extend('force', default_config, config)
   -- path ~/.local/share/nvim
 
-  local cache_dir = vim.fn.stdpath("cache")
-  local outfile = string.format("%s/%s.log", cache_dir,
-                                config.plugin or 'gh')
+  local cache_dir = vim.fn.stdpath('cache')
+  local outfile = string.format('%s%s%s.log', cache_dir, sep(), config.plugin or 'gh')
 
   local obj
   if standalone then
@@ -55,17 +68,17 @@ log.new = function(config, standalone)
   local round = function(x, increment)
     increment = increment or 1
     x = x / increment
-    return (x > 0 and math.floor(x + .5) or math.ceil(x - .5)) * increment
+    return (x > 0 and math.floor(x + 0.5) or math.ceil(x - 0.5)) * increment
   end
 
   local make_string = function(...)
     local t = {}
-    for i = 1, select("#", ...) do
+    for i = 1, select('#', ...) do
       local x = select(i, ...)
 
-      if type(x) == "number" and config.float_precision then
+      if type(x) == 'number' and config.float_precision then
         x = tostring(round(x, config.float_precision))
-      elseif type(x) == "table" then
+      elseif type(x) == 'table' then
         x = vim.inspect(x)
       else
         x = tostring(x)
@@ -73,7 +86,7 @@ log.new = function(config, standalone)
 
       t[#t + 1] = x
     end
-    return table.concat(t, " ")
+    return table.concat(t, ' ')
   end
 
   local log_at_level = function(level, level_config, message_maker, ...)
@@ -84,32 +97,31 @@ log.new = function(config, standalone)
     local nameupper = level_config.name:upper()
 
     local msg = message_maker(...)
-    local info = debug.getinfo(2, "Sl")
-    local lineinfo = info.short_src .. ":" .. info.currentline
+    local info = debug.getinfo(2, 'Sl')
+    local lineinfo = info.short_src .. ':' .. info.currentline
 
     -- Output to console
     if config.use_console then
-      local console_string = string.format("[%-6s%s] %s: %s", nameupper, os.date("%H:%M:%S"),
-                                           lineinfo, msg)
+      local console_string = string.format('[%-6s%s] %s: %s', nameupper, os.date('%H:%M:%S'), lineinfo, msg)
 
       if config.highlights and level_config.hl then
-        vim.cmd(string.format("echohl %s", level_config.hl))
+        vim.cmd(string.format('echohl %s', level_config.hl))
       end
 
-      local split_console = vim.split(console_string, "\n")
+      local split_console = vim.split(console_string, '\n')
       for _, v in ipairs(split_console) do
         vim.cmd(string.format([[echom "[%s] %s"]], config.plugin, vim.fn.escape(v, '"')))
       end
 
       if config.highlights and level_config.hl then
-        vim.cmd("echohl NONE")
+        vim.cmd('echohl NONE')
       end
     end
 
     -- Output to log file
     if config.use_file then
-      local fp = io.open(outfile, "a")
-      local str = string.format("[%-6s%s] %s: %s\n", nameupper, os.date(), lineinfo, msg)
+      local fp = io.open(outfile, 'a')
+      local str = string.format('[%-6s%s] %s: %s\n', nameupper, os.date(), lineinfo, msg)
       fp:write(str)
       fp:close()
     end
@@ -120,9 +132,9 @@ log.new = function(config, standalone)
       return log_at_level(i, x, make_string, ...)
     end
 
-    obj[("fmt_%s"):format(x.name)] = function()
+    obj[('fmt_%s'):format(x.name)] = function()
       return log_at_level(i, x, function(...)
-        local passed = {...}
+        local passed = { ... }
         local fmt = table.remove(passed, 1)
         local inspected = {}
         for _, v in ipairs(passed) do
